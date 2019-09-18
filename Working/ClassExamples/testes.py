51975529c2a2482f117f05893d0cab08a92af8a1
Danielzahu <daniel.zhu22@ycdsbk12.ca>
2019-09-18 08:50:49 -0400

    .

---
+++ Working/ClassExamples/testes.py
@@ -0,0 +1,15 @@
+print("Input first value")
+a = input()
+print("Input second value")
+b = input()
+print("Input third value")
+c = input()
+if a > b and a > c:
+    print("A is the biggest number")
+elif b > a and b > c:
+    print("B is the biggest number")
+elif c > a and c > b:
+    print("Cis the biggest number")
+else:
+    print("They are the same")
+   
