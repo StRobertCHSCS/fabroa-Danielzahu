c7adf2f0af5ed003a8810b41f15ed853d027772a
Danielzahu <daniel.zhu22@ycdsbk12.ca>
2019-09-19 09:12:17 -0400

    September 19

---
+++ Working/ClassExamples/1_2Operators.py
@@ -0,0 +1,17 @@
+print(2+3*4+8+6)
+
+instrument = "kazoo"
+age = 7
+
+print("I have played the " + str(instrument) + " since I was " +
+str(age) + " years old.")
+
+age = 14
+name = "Daniel Zhu"
+print("Hi! My name is " + name + " and I am " + str(age) + " years old.")
+
+Length = 10
+Width = 5
+
+print("Area:" + str(Length*Width))
+print("Perimeter:" + str(2*(Length+Width)))
