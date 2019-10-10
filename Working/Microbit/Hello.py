19a6bc00abe85b7ccee8959ad34e0968c2b47ef7
Danielzahu <daniel.zhu22@ycdsbk12.ca>
2019-10-10 09:44:59 -0400

    HI

--- Working/Microbit/Hello.py
+++ Working/Microbit/Hello.py
@@ -1,19 +1,10 @@
 from microbit import*
+import music
 
-while True:
-    reading = accelerometer.get_x()
-    reading_2 = accelerometer.get_y()
-    if reading < 50 and reading_2 < 50:
-        center = Image("00000:09990:09990:09990:00000")
-        display.show(center)
-    if reading >= 50:
-        right = Image("00000:00999:00999:00999:00000")
-        display.show(right)
-    if reading <= -50:
-        left = Image("00000:99900:99900:99900:00000")
-        display.show(left)
-    if reading_2 >= 50:
-        up = Image("09990:09990:09990:00000:00000")
-        display.show(up)
-    if reading_2 <= -50:
-        down = Image("00000:00000:09990:09990:09990")
+tune = ["E4:4", "G#:4", "C#:1", "B3", "C#4:3", "C#", "B3:2", "E4:3", "E4", 
+        "C#:1", "B3", "C#4:3", "C#", "B3:2", "E4:3", "G#",
+        "C#:1", "B3", "C#4:3", "C#", "C#:1", "B3:2", "E4:3", "E4:3", "E:1",
+        "F#:2", 
+        ]
+music.play(tune)
+
