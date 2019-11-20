e6b1b813ed69ef49095a23996da7b7acba00ec27
Danielzahu <daniel.zhu22@ycdsbk12.ca>
2019-11-20 09:47:55 -0500

    E

---
+++ Working/ClassExamples/2_8_for_loops/2_8_1.py
@@ -0,0 +1,6 @@
+numb = int(input("Input number here: "))
+total = 0
+for i in range(numb + 1):
+    total = total + i
+
+print(total)
---
+++ Working/ClassExamples/2_8_for_loops/2_8_2.py
@@ -0,0 +1,6 @@
+number = int(input("Input number here: "))
+total = 0
+for i in range(1, number + 1, 2):
+    total = total + i
+
+print(total)
