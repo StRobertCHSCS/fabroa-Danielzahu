7e71ab060404f573a3bb504fc6928372b564d036
Danielzahu <daniel.zhu22@ycdsbk12.ca>
2019-12-17 09:35:14 -0500

    Final

---
+++ Working/Microbit/doordecor.py
@@ -0,0 +1,28 @@
+from microbit import *
+
+
+def set_servo_angle(pin, angle):
+    duty = 26 + (angle * 102) / 180
+    pin.write_analog(duty)
+
+
+angle = 90
+set_servo_angle(pin2, angle)
+set_servo_angle(pin1, angle)
+set_servo_angle(pin0, angle)
+
+radio.on()
+while True:
+    new_message = radio.receive()
+    if new_message == "a message":
+        while True:
+            angle += 180
+            set_servo_angle(pin2, angle)
+            set_servo_angle(pin1, angle)
+            set_servo_angle(pin0, angle)
+            sleep(300)
+            angle -= 180
+            set_servo_angle(pin2, angle)
+            set_servo_angle(pin1, angle)
+            set_servo_angle(pin0, angle)
+            sleep(300)
